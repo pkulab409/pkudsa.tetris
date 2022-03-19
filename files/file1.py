e6243651918e4ca91c84f69fb36c6a88a00d65cf
aFlyingOctopus <2529615436@qq.com>
2022-03-20 01:11:01 +0800

    Create file1.py

---
+++ files/file1.py
@@ -0,0 +1,20 @@
+import copy
+class player:
+    def __init__(self,n):
+        pass
+    
+    def output(self,n):
+        a=[]
+        pospos=n.position
+        for x in range(10):
+            if a:
+                break
+            for y in range(15):
+                if a:
+                    break
+                for z in range(4):
+                    if pospos[y][x][z]==1:
+                        a=[x,y,z]
+                        break
+        return a
+                        

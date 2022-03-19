7989a30ca7f2fef24f71a045c37de414edd3e096
cresent-rose <101493282+cresent-rose@users.noreply.github.com>
2022-03-19 17:12:04 +0800

    Delete visualization.py

--- visualization.py
+++
@@ -1,9 +0,0 @@
-def visual(lis):
-    c=reverse(lis)
-    for i in lis:
-        for j in i:
-            if j==0:
-                print("0",end="")
-            else:
-                print("@",end="")
-        print("")

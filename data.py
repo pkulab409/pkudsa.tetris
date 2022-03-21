8c56a6305d1c2811cd849db4a65e848c20c12103
cresent-rose <101493282+cresent-rose@users.noreply.github.com>
2022-03-21 15:28:05 +0800

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-"""This is data module"""
-
-class data:
-    def __init__(self):
-        self.GetTime=None
-        self.GetBoard=None
-        self.GetPack=None
-        self.GetBlock=None
-        self.GetPosition=None

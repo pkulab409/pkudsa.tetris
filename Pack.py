a08f2d337ae3da7ad2e8de19fb7e7cf37cc203c4
aFlyingOctopus <2529615436@qq.com>
2022-03-21 19:35:24 +0800

    Create Pack.py

---
+++ Pack.py
@@ -0,0 +1,13 @@
+"""This is pack module"""
+import random
+
+class Pack:
+    def __init__(self):#定义随机块给出函数
+        self.list = []
+        for i in range(81):
+            x = [1,2,3,4,5,6,7]
+            random.shuffle(x)
+            self.list += x
+    
+    def pop(self):#弹出一个块
+        return self.list.pop()

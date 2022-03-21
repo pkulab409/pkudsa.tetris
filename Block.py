d9d13b73f0794659ac3577693af5d661d3da3447
aFlyingOctopus <2529615436@qq.com>
2022-03-21 19:26:00 +0800

    Update Block.py

--- Block.py
+++ Block.py
@@ -1,7 +1,7 @@
 """This is block module"""
 
 #形状的type参量的数据范围是1~7,direc的四个方向按照规则文档定义
-class block:
+class Block:
     offsetTable = [[],
         [  # I型
             [(-1, 0), (0, 0), (1, 0), (2, 0)],
@@ -61,7 +61,7 @@ class block:
         self.x = x
 
     def showblock(self):  # 定义返回各方块位置操作
-        offsets = block.offsetTable[self.type][self.direc]
+        offsets = Block.offsetTable[self.type][self.direc]
         return [
             (self.x + offsets[0][0], self.y + offsets[0][1]),
             (self.x + offsets[1][0], self.y + offsets[1][1]),

9857d6066267d86dad78555865969b7e6956642e
cresent-rose <101493282+cresent-rose@users.noreply.github.com>
2022-04-05 12:38:32 +0800

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,50 @@
+"""This is board module"""
+import Block
+
+class Board:
+    def __init__(self, PeaceAreaWidth = 10, BattleAreaWidth = 5):#定义面板类
+        self.list=[[0 for i in range(10)] for i in range(25)]
+        self.PeaceAreaWidth = PeaceAreaWidth
+        self.BattleAreaWidth = BattleAreaWidth
+
+    def erase(self):    #定义消去行的操作
+        line1 = 0    #用于返回和平区消行数量
+        line2 = 0    #用于返回战争区消行数量
+        empty = False    #用于判断清空奖励(虽然在2.1版本的规则里已经没有这一条了emmm,而且就目前的AI模拟情况来看,好像没人能清空?)
+
+        part1 = self.list[0:self.PeaceAreaWidth + self.BattleAreaWidth]
+        part2 = self.list[self.PeaceAreaWidth + self.BattleAreaWidth:self.PeaceAreaWidth*2 + self.BattleAreaWidth]
+        dellist = []
+        for i in range(self.PeaceAreaWidth):
+            if 0 not in part1[i]:
+                dellist.append(i)
+                line1 += 1
+        for i in range(self.PeaceAreaWidth, self.PeaceAreaWidth + self.BattleAreaWidth):
+            if 0 not in part1[i]:
+                dellist.append(i)
+                line2 += 1
+        while dellist:
+            part1.pop(dellist.pop())
+
+        part1 = [[0 for i in range(10)] for j in range(line1 + line2)] + part1
+        self.list = part1 + part2
+        if line2 == self.BattleAreaWidth:
+            empty = True
+
+        return (line1,line2,empty)
+
+
+    def writein(self,y,x,direction,type):#在某个块生效时写入到面板里
+        test=Block.Block(type,direction)
+        test.move(x,y)
+        for x,y in test.showblock():
+            if y>=0:
+                self.list[y][x]=type
+
+    def reverse(self):
+        for row in self.list:
+            row.reverse()
+        self.list.reverse()
+
+    def clear(self):
+        self.list=[[0 for i in range(10)] for i in range(25)]
---
+++ main.py
@@ -0,0 +1,244 @@
+import Board
+import Pack
+import time
+import MatchData
+import copy
+
+#奖励字典
+peacepoint = {0:0,1:0,2:1,3:2,4:4}
+battlepoint = {0:0,1:1,2:2,3:4,4:8}
+
+#棋盘属性
+PeaceAreaWidth = 10    #和平区行数
+BattleAreaWidth = 5    #战斗区行数
+
+#先手玩家的编号是first，玩家2的编号是last
+#棋盘的左上角为坐标原点(0,0)
+#七种方块的种类和角度按照规则文档提供,编号1~7
+#玩家需要实现一个ai类，ai类有两个方法：
+    #方法1：通过接受bool表示的是否先攻创建实例
+    #方法2：通过调取无变量函数"self.output()"给出元组：
+        #(y:int,x:int,position:int)
+
+
+class Game:
+    def __init__(self, teamfirst, teamlast, limit = 9999):
+        #创建一个供玩家调用数据和方法的类
+        self.matchdata = MatchData.MatchData()
+        #定义游戏类的各种属性，limit是每回合时间限制，team是玩家队伍和文件名
+        self.block = -1    #本回合方块
+        self.teamname = [teamfirst,teamlast]
+        self.state = "gaming"
+        self.time1 = limit    #玩家1剩余时间
+        self.time2 = limit    #玩家2剩余时间
+        self.board = Board.Board(PeaceAreaWidth, BattleAreaWidth)    #棋盘
+        self.pack = Pack.Pack()    #全部块数
+        self.winner = -1
+        self.combo = 0
+        self.removeline = False     #用于战斗区连击判定
+        self.point1 = 0    #玩家1得分
+        self.point2 = 0    #玩家2得分
+        self.player = []
+        self.time = 0    #游戏进行轮次(并非回合数!!!)
+        self.pcleartimes=[0,0,0,0,0]
+        self.bcleartimes=[0,0,0,0,0]
+        self.combocount=0
+        self.isFirst=0
+        self.round=0
+        
+        #实现游戏文件的存储功能
+        self.boardsave=[]
+        self.actionsave=[]
+        self.scoresave=[]
+
+        #读入玩家程序,读不到判负
+        try:
+            exec("""from files.{} import Player as playerfirst
+self.player.append(playerfirst(True))""".format(teamfirst))
+        except:
+            self.winner = 2
+            print("p1 ai missing")
+            self.state = "judge to end"
+        try:
+            exec("""from files.{} import Player as playerlast
+self.player.append(playerlast(False))""".format(teamlast))
+        except:
+            if self.state == "judge to end":
+                print("p2 ai missing")
+                self.winner = -1
+            else:
+                self.winner = 1
+                self.state = "judge to end"
+                print("p2 ai missing")
+    
+
+    #同步用户可调用数据
+    def updateData(self):
+        self.matchdata.block = self.block
+        self.matchdata.time1 = self.time1
+        self.matchdata.time2 = self.time2
+        self.matchdata.board = copy.deepcopy(self.board.list)
+        self.matchdata.pack = self.pack
+        self.matchdata.point1 = self.point1
+        self.matchdata.point2 = self.point2
+        self.matchdata.time = self.time
+        self.matchdata.combo = self.combo
+
+
+    #定义每个回合都要进行的游戏
+    def turn(self):
+        self.time += 1
+
+        if self.time > 560:
+            self.state = "round limit"    #达到回合数上限
+
+        self.block = self.pack.get(self.time)    #取出下一块
+
+        #调用output方法返回一个列表
+        #监督对于时间的使用和是否返回报错
+        if self.time%2 == 1:    #先手玩家操作
+            self.isFirst == True
+            self.round += 1
+            #p1 有效落块位置
+            validpos = self.matchdata.getAllValidAction(self.block, self.board.list)
+            if len(validpos)==0:    #p1 无路可走,溢出
+                self.state = 'p1 overflow'
+                self.winner = 2
+                return None
+
+            #更新资料包,准备发送给p1
+            self.matchdata.isFirst = True
+            self.updateData()
+
+
+            T1 = time.time()
+            #try:
+            action = self.player[0].output(self.matchdata)
+            """except Exception:    #p1 程序出错
+                print("p1 ai error!")
+                self.state = 'p1 error'
+                self.winner = 2
+                return None"""
+            T2 = time.time()
+
+            #时间判定
+            if self.time1 < T2-T1:    #p1 超时
+                print("p1 ai overtime")
+                self.state = 'p1 overtime'
+                self.winner = 2
+            else:
+                self.time1 -= T2-T1
+
+            #合法性判定
+            if action in validpos:
+                self.board.writein(action[0],action[1],action[2],self.block)
+            else:    #p1 非法落块
+                print("p1 ai illegal")
+                self.state = "judge to end"
+                self.winner = 2
+                return None
+
+            #清理满行
+            peaceline, battleline, empty = self.board.erase()
+            self.pcleartimes[peaceline]+=1
+            self.bcleartimes[battleline]+=1
+
+            #计算分数
+            if battleline:
+                self.removeline = True
+                self.point1 += battlepoint[battleline] + peacepoint[peaceline] +  self.combo
+            else:
+                self.point1 += peacepoint[peaceline]
+
+
+        else:    #后手玩家操作
+
+            #后手玩家需要翻转棋盘
+            self.board.reverse()    
+            self.isFirst == False
+            #p2 有效落块位置
+            validpos = self.matchdata.getAllValidAction(self.block, self.board.list)
+            if len(validpos)==0:    #p2 无路可走,溢出
+                self.state = 'p2 overflow'
+                self.winner = 1
+                return None
+
+            #更新资料包,准备发送给p2
+            self.matchdata.isFirst = False
+            self.updateData()
+
+
+            T1 = time.time()
+            try:
+                action = self.player[1].output(self.matchdata)
+            
+            except Exception:    #p2 程序出错
+                print("p2 ai error!")
+                self.state = 'p2 error'
+                self.winner = 1
+                return None
+            
+            T2 = time.time()
+            
+
+            #时间判定
+            if self.time2 < T2-T1:    #p2 超时
+                print("p2 ai overtime")
+                self.state = 'p2 overtime'
+                self.winner = 1
+            else:
+                self.time2 -= T2-T1
+
+            #合法性判定            
+            if action in validpos:
+                self.board.writein(action[0],action[1],action[2],self.block)
+            else:    #p2 非法落块
+                print("p2 ai illegal")
+                self.state = "judge to end"
+                self.winner = 1
+                return None
+
+            #清理满行
+            peaceline, battleline, empty = self.board.erase()
+            self.pcleartimes[peaceline]+=1
+            self.bcleartimes[battleline]+=1
+
+            #计算分数
+            if battleline:
+                self.removeline = True
+                self.point2 += battlepoint[battleline] + peacepoint[peaceline] +  self.combo
+            else:
+                self.point2 += peacepoint[peaceline]
+
+            #把棋盘翻转回去
+            self.board.reverse()
+
+            #连击结算
+            if self.removeline:
+                self.combo += 1
+            else:
+                self.combo = 0
+            self.removeline = False
+
+
+    #游戏结束的广播
+    def end(self):
+        print("本局游戏结束")
+        if self.state == 'round limit':
+            if self.point1 > self.point2:
+                self.winner = 1
+            elif self.point1 < self.point2:
+                self.winner = 2
+            else:
+                self.winner = "平局"
+        print("胜者是",self.winner)
+        print("游戏结束原因是",self.state)
+        print(self.time)
+
+
+if __name__ == "__main__":
+    for i in range(100):
+        play = Game("file1","file2",100)
+        while play.state=="gaming":
+            play.turn()
+        play.end()

c46e2c9b420bae8e52777d2bab430dd9a325c5d2
LRLVEC <mjdyx@outlook.com>
2022-04-14 22:07:00 +0800

    rename getAllValidActionRepeating

--- TetrisProgram/MatchData.py
+++ TetrisProgram/MatchData.py
@@ -25,11 +25,11 @@ class MatchData:
                 return False
         return True
     
-    def getAllValidAction(self, type, board): # 返回值是允许的位置的三元组 (y, x, pos) 的列表
-        return CppAcceleration.GetAllValidAction(type, board)
+    def getAllValidActionRepeating(self, type, board): # 返回值是允许的位置的三元组 (y, x, pos) 的列表
+        return CppAcceleration.GetAllValidActionRepeating(type, board)
 
-    def getAllValidActionNonRepeating(self, type, board): # 返回值是操作后结果不重复的允许的位置的三元组 (y, x, pos) 的列表
-        return CppAcceleration.GetAllValidActionNonRepeating(type, board)
+    def getAllValidAction(self, type, board): # 返回值是操作后结果不重复的允许的位置的三元组 (y, x, pos) 的列表
+        return CppAcceleration.GetAllValidAction(type, board)
 
     def getAllValidActionSlow(self, type, board):    #寻找所有可能位置,默认层数是3
         layers = 10

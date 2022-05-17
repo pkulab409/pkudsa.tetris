70646ef8afd262f3af18b96d1daf5699c944aca2
aFlyingOctopus <2529615436@qq.com>
2022-05-18 01:04:29 +0800

    Update Pack.py

--- TetrisProgram/Pack.py
+++ TetrisProgram/Pack.py
@@ -4,7 +4,7 @@ import random
 class Pack:
     def __init__(self):#定义随机块给出函数
         self.list = [0]#为保证时间和列表不冲突，先加入1个占位元
-        for i in range(81):
+        for i in range(80):
             x = [1,2,3,4,5,6,7]
             random.shuffle(x)
             self.list += x

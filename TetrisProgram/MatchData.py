de229710e20d1eb2c054020fa6b9525488e3b631
LRLVEC <mjdyx@outlook.com>
2022-05-23 02:06:31 +0800

    add getValidActionRepeatingSlow

--- TetrisProgram/MatchData.py
+++ TetrisProgram/MatchData.py
@@ -54,13 +54,70 @@ class MatchData:
     def getAllValidActionRepeating(self, type, board): # 返回值是允许的位置的三元组 (y, x, pos) 的列表
         if CppAcceleration:
             return CppAcceleration.GetAllValidActionRepeating(type, board)
-        return self.getAllValidActionSlow(type, board)
+        return self.getAllValidActionRepeatingSlow(type, board)
 
     def getAllValidAction(self, type, board): # 返回值是操作后结果不重复的允许的位置的三元组 (y, x, pos) 的列表
         if CppAcceleration:
             return CppAcceleration.GetAllValidAction(type, board)
         return self.getAllValidActionSlow(type, board)
 
+    def getAllValidActionRepeatingSlow(self, type, board): # 返回值是允许的位置的三元组 (y, x, pos) 的列表
+
+        def judge(pos,board): # 判定位置是否碰撞
+            for x,y in pos:
+                if x<0 or x>9 or y>14:
+                    return False
+                elif y>=0 and board[y][x]!=0:
+                    return False
+            return True
+
+        layers = 10
+        validboard=[[[1 for i in range(4)] for i in range(10)] for i in range(15)]
+        test=Block.Block(type,0)
+        for pos in range(4):
+            test.rotateto(pos)
+            for x in range(10):
+                for y in range(15):
+                    test.move(x,y)
+                    validboard[y][x][pos]=judge(test.showblock(),board)
+        phaseboard=[validboard[0]]+[[[0 for i in range(4)] for i in range(10)] for i in range(14)]
+        for y in range(1,15):
+            for x in range(10):#从上方继承
+                for pos in range(4):
+                    if validboard[y][x][pos] and phaseboard[y-1][x][pos]:
+                        phaseboard[y][x][pos]=1
+            for i in range(layers):#水平继承，layer是搜索深度
+                for x in range(1,10):#左继承
+                    for pos in range(4):
+                        if phaseboard[y][x-1][pos] and validboard[y][x][pos]:
+                            phaseboard[y][x][pos]=1
+                for x in range(0,9):#右继承
+                    for pos in range(4):
+                        if phaseboard[y][9-x][pos] and validboard[y][8-x][pos]:
+                            phaseboard[y][8-x][pos]=1
+                for x in range(0,9):#旋转继承
+                    if 1 in phaseboard[y][x]:
+                        phaseboard[y][x]=list(validboard[y][x])
+        
+        for pos in range(4):#确认是否可以为最终位置
+            for x in range(10):
+                for y in range(14):
+                    if validboard[y+1][x][pos]:
+                        phaseboard[y][x][pos]=0
+                    if y==0:
+                        if (type,pos) in [(1,1),(1,3),(2,0),(2,1),(2,3),(3,0),(3,1),(3,3),(4,0),(4,3),(5,0),(5,1),(5,3),(6,0),(6,1),(6,3),(7,0),(7,1),(7,3)]:
+                            phaseboard[y][x][pos]=0
+                    if y==1 and type==1 and pos==3:
+                        phaseboard[y][x][pos]=0 
+        phaseboard += [[[0 for i in range(4)]for i in range(10)]for i in range(10)]
+        result = []
+        for y in range(14, -1, -1):
+            for x in range(10):
+                for pos in range(4):
+                    if phaseboard[y][x][pos]:
+                        result.append((y, x, pos))
+        return result
+        
     def getAllValidActionSlow(self, type, board):    #寻找所有可能位置
 
         def judge(pos,board): # 判定位置是否碰撞
